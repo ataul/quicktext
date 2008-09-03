45ef69d488a3bb3e3c8ffc0b9726de13e7951554
ataul.haque <ataul.haque@ef205d63-e045-0410-a319-b7ccb1caf051>
2008-09-03 01:19:28 +0000

    Converter for all editor

---
+++ tools/AConv/io/FileOp.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package AConv.io;
+
+import java.io.*;
+
+/**
+ *
+ * @author Ataul
+ * @version 2.0
+ * @first_created May 13, 2008
+ * @last_modified May 13, 2008
+ */
+public class FileOp {
+        private FileOutputStream fout;	
+
+	public void OpenFile(String FileName)
+	{	
+		try{
+			fout = new FileOutputStream (FileName,true);					
+		}
+		catch(Exception e)
+		{
+		
+		}				
+	}
+	
+	public void writeFile(String data)
+	{		
+		new PrintStream(fout).println(data);		
+	}
+	
+	public void CloseFile(){
+		try{
+			fout.close();			
+		}
+		catch(Exception e)
+		{
+		
+		}		
+	}
+
+
+}
