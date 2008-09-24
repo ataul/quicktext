d01baf5bfe2199fb7d5745d909aae4f295a223c3
ataul.haque <ataul.haque@ef205d63-e045-0410-a319-b7ccb1caf051>
2008-09-24 01:19:05 +0000

    docid and ce for javascript.js Converter still not finished

--- tools/AConv/Conv.java
+++ tools/AConv/Conv.java
@@ -1,23 +1,31 @@
 /**
  * @author Ataul
  * @first_created September 3, 2008
- * @last_updated September 3, 2008
+ * @last_updated September 24, 2008
  */
 
 package AConv;
  
-import io.*; 
+import AConv.io.*; 
+import java.io.*;
+import java.util.*;
+
 public class Conv {
 
 	String Abbreviation[];
 	String Code[];
 	String b;
+	String version;
+	FileOp file;
 	
 	public Conv(){
 		b = new String();
+		version = "0.2";
+		this.file = new FileOp();
 	}
 	
-	public void toCodePress(){
+	public void toCodePress(String FileName){
+		file.OpenFile(FileName);
 		for(int i=0;i<Abbreviation.length;i++){
 			b = "{ input : '";
 			b+=Abbreviation[i];
@@ -26,19 +34,22 @@ public class Conv {
 			b+="' },";
 			file.writeFile(b);
 		}
-		
+		file.CloseFile();
 	}
 	
-	public void toNPP(){
+	public void toNPP(String FileName){
+		file.OpenFile(FileName);
 		for(int i=0;i<Abbreviation.length;i++){
 			b=Abbreviation[i];
 			b+="=";
 			b+=Code[i];			
 			file.writeFile(b);
-		}		
+		}
+		file.CloseFile();	
 	}
 	
-	public void toNetBeans(){
+	public void toNetBeans(String FileName){
+		file.OpenFile(FileName);
 		for(int i=0;i<Abbreviation.length;i++){
 			b = "<codetemplate abbreviation=\"";
 			b+=Abbreviation[i];
@@ -51,11 +62,48 @@ public class Conv {
 			b="</codetemplate>";
 			file.writeFile(b);
 		}
-		
+		file.CloseFile();
 	}
-	
+	public void getDataConsole()
+	{
+		System.out.println("/*****************************************************/");
+		System.out.println("/*                 AConv                   */");
+		System.out.println("/*                 Version"+version+"                 */");
+		System.out.println("/*             Created by: Atul               */");
+		System.out.println("/*****************************************************/");
+		try{
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			String input=new String();		
+			Vector v=new Vector();
+			do
+			{
+				input=in.readLine();
+				v.addElement(input);			
+			}
+			while(!input.equals(""));
+			
+			String[] arr=new String[v.size()-1];
+			String[] arr2=new String[v.size()-1];
+			for(int i=0;i<v.size()-1;i++)
+			{
+				String mixed = (String) v.elementAt(i);
+				String[] mixedArray = mixed.split("|");
+				arr[i]=mixedArray[0];
+				arr2[i] = mixedArray[1];
+			}
+			Abbreviation=arr;
+			Code = arr2;
+			toNPP("QuickText.ini");
+			toCodePress("php.js");
+			toNetBeans("a.xml");
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
 	public static void main(String args[]){
-	
+		Conv a = new Conv();
+		a.getDataConsole();
 	}
 	
 };
